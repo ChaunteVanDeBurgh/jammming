5701b32164f739957c3c0a22da9dd2742dd47dc8
ChaunteVanDeBurgh <chauntevandeburgh@gmail.com>
2019-02-28 16:35:57 -0600

    style Track component

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -9,7 +9,7 @@ class Track extends React.Component {
     			<h3>// track name will go here </h3>
     			<p>// track artist will go here | track album will go here </p>
   			</div>
- 				<a class="Track-action"><!-- + or - will go here --></a>
+ 				<a class="Track-action">// + or - will go here </a>
 			</div>
 		)
 	}
