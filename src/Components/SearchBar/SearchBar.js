a146b93405d44eb772a68776a1a4f583264e5dd5
ChaunteVanDeBurgh <chauntevandeburgh@gmail.com>
2019-03-16 00:04:43 -0500

    create handleTermChange method

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,16 +5,21 @@ class SearchBar extends React.Component {
 	constructor(props) {
 		super(props);
 		this.search = this.search.bind(this);
+		this.handleTermChange = this.search.bind(this);
 	}
 
 	search(term) {
 		this.props.onSearch(term);
 	}
 
+	handleTermChange(e) {
+		const newTerm = e.target.value;
+	}
+
 	render() {
 		return (
 			<div className="SearchBar">
-  			<input placeholder="Enter A Song, Album, or Artist" />
+  			<input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
   			<a>SEARCH</a>
 			</div>
 		)
